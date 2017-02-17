63d85c94a10b8f17c423c6ad29991060fba8a64a
vipulperiwal <vipul.periwal@gmail.com>
2017-02-17 18:47:06 -0500

    save changes
    probably gonna start a new branch cause this is glitchy af

--- src/org/usfirst/frc5265/steamwiffle/commands/AutonomousCommand.java
+++ src/org/usfirst/frc5265/steamwiffle/commands/AutonomousCommand.java
@@ -43,6 +43,10 @@ public class AutonomousCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+  
+    	
+    	
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
