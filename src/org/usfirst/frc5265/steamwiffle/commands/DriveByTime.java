8e5b38cba9d2ef1be0712eff15f74895f9638be6
gmnielsen <nielsen@cheverus.org>
2017-02-19 10:39:20 -0500

    drive by time end
    called motion to drop to 0,0,0

--- src/org/usfirst/frc5265/steamwiffle/commands/DriveByTime.java
+++ src/org/usfirst/frc5265/steamwiffle/commands/DriveByTime.java
@@ -31,11 +31,12 @@ public class DriveByTime extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return isTimedOut();
+    	return isTimedOut();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.chassis.driveChassisSteering(0, 0, 0);
     }
 
     // Called when another command which requires one or more of the same
